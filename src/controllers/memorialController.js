434235311dcd71ca3e1dbc15d58efe6bd08fa87c
Vincent Iroleh <irolehiroleh@gmail.com>
2020-07-22 22:10:30 +0100

    chore(add resources memorial endpoint) add memorial endpoint resources

--- src/controllers/memorialController.js
+++ src/controllers/memorialController.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 const Memorial = require('../models/memorialModel');
 const User = require('../models/userModel');
 
 class memorialController {
   static createMemorial(req, res) {
-    const id = { _id: req.body.user };
+    const id = { _id: req.body.userID };
     const value = { ...req.body, user: id, image: req.file.path };
 
     User.findOne(id)
@@ -21,6 +22,87 @@ class memorialController {
           message: err.message,
         });
       });
+  }
+
+  static getMemorials(req, res) {
+    Memorial.find({})
+      .populate('user')
+      .then((memorials) => {
+        res.status(200).json({
+          status: 200,
+          message: 'Memorials retrieved successfully',
+          memorials,
+        });
+      }).catch((err) => {
+        res.status(404).json({
+          status: 400,
+          message: err.message,
+        });
+      });
+  }
+
+  static getMemorial(req, res) {
+    const webAddress = { webAddress: req.params.webAddress };
+    Memorial.findOne(webAddress)
+      .populate('user')
+      .then((memorial) => {
+        res.status(200).json({
+          status: 200,
+          message: 'Memorial retrieved successfully',
+          memorial,
+        });
+      }).catch((err) => {
+        res.status(400).json({
+          status: 400,
+          message: err.message,
+        });
+      });
+  }
+
+  static getUserMemorials(req, res) {
+    Memorial.find({ user: req.user._id })
+      .populate('user')
+      .then((memorials) => {
+        res.status(200).json({
+          status: 200,
+          message: 'Memorials retrieved successfully',
+          memorials,
+        });
+      }).catch((err) => {
+        res.status(400).json({
+          status: 400,
+          message: err.message,
+        });
+      });
+  }
+
+  static updateMemorial(req, res) {
+    const id = { _id: req.body.id };
+    const value = { ...req.body, user: id, image: req.file.path };
+    Memorial.updateOne(id, {
+      $set: value,
+    })
+      .then(() => res.status(200).json({
+        status: 200,
+        message: 'Details updated successfully',
+      }))
+      .catch((err) => res.status(400).json({
+        status: 400,
+        message: err.message,
+      }));
+  }
+
+  static deleteMemorial(req, res) {
+    const memorialId = { _id: req.body.id };
+    Memorial.deleteOne(memorialId)
+      .then(() => res.status(200).json({
+        status: 200,
+        message: 'Memorial page removed successfully',
+      }))
+      .catch((err) => res.status(400).json({
+        status: 400,
+        message: err.message,
+      }));
   }
 }
 
