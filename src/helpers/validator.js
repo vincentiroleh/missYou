a606650ee6fea75a79b8bfbd63d4600deb428464
Restyled.io <commits@restyled.io>
2020-07-24 14:46:51 +0000

    Restyled by prettier

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -1,39 +1,47 @@
-const {body, validationResult} = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
-const signupValidator =
-    () => [body('name').isString().not().isEmpty().trim().withMessage(
-               'name cannot be empty'),
-           body('email').isEmail().trim().withMessage(
-               'email must be valid and cannot be empty'),
-           body('password')
-               .isLength({min : 5})
-               .trim()
-               .withMessage('password must be at least 5 chars long'),
+const signupValidator = () => [
+  body("name")
+    .isString()
+    .not()
+    .isEmpty()
+    .trim()
+    .withMessage("name cannot be empty"),
+  body("email")
+    .isEmail()
+    .trim()
+    .withMessage("email must be valid and cannot be empty"),
+  body("password")
+    .isLength({ min: 5 })
+    .trim()
+    .withMessage("password must be at least 5 chars long"),
 ];
 
-const loginValidator =
-    () => [body('email').isEmail().trim().withMessage(
-               'email must be valid and cannot be empty'),
-           body('password')
-               .not()
-               .isEmpty()
-               .trim()
-               .withMessage('password field cannot be empty'),
+const loginValidator = () => [
+  body("email")
+    .isEmail()
+    .trim()
+    .withMessage("email must be valid and cannot be empty"),
+  body("password")
+    .not()
+    .isEmpty()
+    .trim()
+    .withMessage("password field cannot be empty"),
 ];
 
-const validate = ((req, res, next) => {
+const validate = (req, res, next) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) {
     return next();
   }
   const extractedErrors = [];
-  errors.array().map((err) => extractedErrors.push({[err.param] : err.msg}));
+  errors.array().map((err) => extractedErrors.push({ [err.param]: err.msg }));
 
   return res.status(422).json({
-    status : 422,
-    errors : extractedErrors,
+    status: 422,
+    errors: extractedErrors,
   });
-});
+};
 
 module.exports = {
   signupValidator,
