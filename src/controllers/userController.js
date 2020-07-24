12bf37ef4f63ffe49ed05e4a1d024b4456225900
Restyled.io <commits@restyled.io>
2020-07-24 14:46:47 +0000

    Restyled by clang-format

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,9 @@ const User = require('../models/userModel');
 class UserController {
   static createUser(req, res) {
     const {
-      name, email, password,
+      name,
+      email,
+      password,
     } = req.body;
 
     const userData = {
@@ -16,34 +18,40 @@ class UserController {
     User.register(userData, password, (err) => {
       if (err) {
         return res.status(400).json({
-          status: 400,
-          message: err.message,
+          status : 400,
+          message : err.message,
         });
       }
-      return passport.authenticate('local')(req, res, () => res.status(200).json({
-        status: 200,
-        message: 'Account created successfully',
-        data: userData,
-      }));
+      return passport.authenticate('local')(
+          req, res, () => res.status(200).json({
+            status : 200,
+            message : 'Account created successfully',
+            data : userData,
+          }));
     });
   }
 
   static loginUser(req, res, next) {
     passport.authenticate('local', (err, user) => {
-      if (err) { return next(err); }
+      if (err) {
+        return next(err);
+      }
       if (!user || user.role === 'Admin') {
         return res.status(400).json({
-          status: 400,
-          message: 'failure',
-          error: 'Invalid email or password',
+          status : 400,
+          message : 'failure',
+          error : 'Invalid email or password',
         });
       }
-      return req.logIn(user, { session: false }, (error) => {
-        if (error) { return next(err); }
-        const token = jwt.sign({ user }, process.env.SECRET, { expiresIn: '168h' });
+      return req.logIn(user, {session : false}, (error) => {
+        if (error) {
+          return next(err);
+        }
+        const token =
+            jwt.sign({user}, process.env.SECRET, {expiresIn : '168h'});
         return res.status(200).json({
-          status: 200,
-          message: 'Authenticated',
+          status : 200,
+          message : 'Authenticated',
           token,
           user,
         });
@@ -54,8 +62,8 @@ class UserController {
   static logoutUser(req, res) {
     req.logout();
     res.status(200).json({
-      status: 200,
-      message: 'Successfully logged out',
+      status : 200,
+      message : 'Successfully logged out',
     });
   }
 }
