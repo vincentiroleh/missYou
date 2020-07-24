ed8f03d1bedd7ddb4d58355538d923da23756aab
Restyled.io <commits@restyled.io>
2020-07-24 14:46:51 +0000

    Restyled by prettier

--- src/middleware/AuthUser.js
+++ src/middleware/AuthUser.js
@@ -1,25 +1,25 @@
 /* eslint-disable prefer-destructuring */
 /* eslint-disable consistent-return */
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 function isAuthUser(req, res, next) {
   if (req.isAuthenticated()) {
     next();
   }
   return res.status(401).send({
-    status : 401,
-    message : 'Authorization required',
+    status: 401,
+    message: "Authorization required",
   });
 }
 
 function isAdmin(req, res, next) {
   const user = req.user;
-  if (user.role === 'Admin') {
+  if (user.role === "Admin") {
     next();
   } else {
     return res.status(401).send({
-      status : 401,
-      message : 'Authorization required',
+      status: 401,
+      message: "Authorization required",
     });
   }
 }
@@ -27,12 +27,12 @@ function isAdmin(req, res, next) {
 function isClient(req, res, next) {
   const user = req.user;
   console.log(user);
-  if (user.role === 'Client') {
+  if (user.role === "Client") {
     next();
   } else {
     return res.status(401).send({
-      status : 401,
-      message : 'Authorization required',
+      status: 401,
+      message: "Authorization required",
     });
   }
 }
@@ -41,17 +41,17 @@ function verifyToken(req, res, next) {
   let token = req.headers.authorization;
   if (!token) {
     return res.status(403).json({
-      auth : false,
-      error : 'No token provided',
+      auth: false,
+      error: "No token provided",
     });
   }
-  token = token.split(' ')[1];
+  token = token.split(" ")[1];
   // verify the token and expire
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).json({
-        auth : false,
-        message : 'Failed to authenticated token',
+        auth: false,
+        message: "Failed to authenticated token",
       });
     }
     req.user = decoded.user;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.use(require('./userRoute'));
-router.use(require('./memorialRoute'));
+router.use(require("./userRoute"));
+router.use(require("./memorialRoute"));
 
 module.exports = router;
