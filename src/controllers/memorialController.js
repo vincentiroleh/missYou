9efb8a733603a1716951f1e6f3e7872b904f056c
Vincent Iroleh <irolehiroleh@gmail.com>
2020-07-27 14:06:49 +0100

    Revert "Restyled by prettier"
    This reverts commit b36e621628962757ebd76eda6aa0c81585c041cd.

--- src/controllers/memorialController.js
+++ src/controllers/memorialController.js
@@ -1,120 +1,111 @@
 /* eslint-disable no-underscore-dangle */
-const Memorial = require("../models/memorialModel");
-const User = require("../models/userModel");
+const Memorial = require('../models/memorialModel');
+const User = require('../models/userModel');
 
 class memorialController {
   static createMemorial(req, res) {
-    const id = { _id: req.body.userID };
-    const value = { ...req.body, user: id, image: req.file.path };
+    const id = {_id : req.body.userID};
+    const value = {...req.body, user : id, image : req.file.path};
 
     User.findOne(id)
-      .then(() =>
-        Memorial.create(value).then((data) => {
+        .then(() => Memorial.create(value).then((data) => {
           res.status(200).json({
-            status: 200,
-            message: `${data.firstname}'s memorial page created successfully`,
+            status : 200,
+            message : `${data.firstname}'s memorial page created successfully`,
             data,
           });
-        })
-      )
-      .catch((err) => {
-        res.status(400).json({
-          status: 400,
-          message: err.message,
+        }))
+        .catch((err) => {
+          res.status(400).json({
+            status : 400,
+            message : err.message,
+          });
         });
-      });
   }
 
   static getMemorials(req, res) {
     Memorial.find({})
-      .populate("user")
-      .then((memorials) => {
-        res.status(200).json({
-          status: 200,
-          message: "Memorials retrieved successfully",
-          memorials,
+        .populate('user')
+        .then((memorials) => {
+          res.status(200).json({
+            status : 200,
+            message : 'Memorials retrieved successfully',
+            memorials,
+          });
+        })
+        .catch((err) => {
+          res.status(404).json({
+            status : 400,
+            message : err.message,
+          });
         });
-      })
-      .catch((err) => {
-        res.status(404).json({
-          status: 400,
-          message: err.message,
-        });
-      });
   }
 
   static getMemorial(req, res) {
-    const id = { _id: req.params.id };
+    const id = {_id : req.params.id};
     Memorial.findOne(id)
-      .populate("user")
-      .then((memorial) => {
-        res.status(200).json({
-          status: 200,
-          message: "Memorial retrieved successfully",
-          memorial,
+        .populate('user')
+        .then((memorial) => {
+          res.status(200).json({
+            status : 200,
+            message : 'Memorial retrieved successfully',
+            memorial,
+          });
+        })
+        .catch((err) => {
+          res.status(400).json({
+            status : 400,
+            message : err.message,
+          });
         });
-      })
-      .catch((err) => {
-        res.status(400).json({
-          status: 400,
-          message: err.message,
-        });
-      });
   }
 
   static getUserMemorials(req, res) {
-    Memorial.find({ user: req.user._id })
-      .populate("user")
-      .then((memorials) => {
-        res.status(200).json({
-          status: 200,
-          message: "Memorials retrieved successfully",
-          memorials,
+    Memorial.find({user : req.user._id})
+        .populate('user')
+        .then((memorials) => {
+          res.status(200).json({
+            status : 200,
+            message : 'Memorials retrieved successfully',
+            memorials,
+          });
+        })
+        .catch((err) => {
+          res.status(400).json({
+            status : 400,
+            message : err.message,
+          });
         });
-      })
-      .catch((err) => {
-        res.status(400).json({
-          status: 400,
-          message: err.message,
-        });
-      });
   }
 
   static updateMemorial(req, res) {
-    const id = { _id: req.params.id };
-    const value = { ...req.body, image: req.file.path };
-    Memorial.updateOne(id, {
-      $set: value,
-    })
-      .then(() =>
-        res.status(200).json({
-          status: 200,
-          message: "Memorial Page updated successfully",
+    const id = {_id : req.params.id};
+    const value = {...req.body, image : req.file.path};
+    Memorial
+        .updateOne(id, {
+          $set : value,
         })
-      )
-      .catch((err) =>
-        res.status(404).json({
-          status: 404,
-          message: err.message,
-        })
-      );
+        .then(() => res.status(200).json({
+          status : 200,
+          message : 'Memorial Page updated successfully',
+        }))
+        .catch((err) => res.status(404).json({
+          status : 404,
+          message : err.message,
+        }));
   }
 
   static deleteMemorial(req, res) {
-    const memorialId = { _id: req.body.id };
+    const memorialId = {_id : req.body.id};
     Memorial.deleteOne(memorialId)
-      .then(() =>
-        res.status(200).json({
-          status: 200,
-          message: "Memorial page removed successfully",
-        })
-      )
-      .catch((err) =>
-        res.status(400).json({
-          status: 400,
-          message: err.message,
-        })
-      );
+        .then(() => res.status(200).json({
+          status : 200,
+          message : 'Memorial page removed successfully',
+        }))
+        .catch((err) => res.status(400).json({
+          status : 400,
+          message : err.message,
+        }));
   }
 }
 
--- src/helpers/cloudinary.js
+++ src/helpers/cloudinary.js
@@ -1,16 +1,14 @@
-const multer = require("multer");
-const cloudinary = require("cloudinary").v2;
-const cloudinaryStorage = require("multer-storage-cloudinary");
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const cloudinaryStorage = require('multer-storage-cloudinary');
 
 const storage = cloudinaryStorage.createCloudinaryStorage({
   cloudinary,
-  folder: "memorial",
-  allowedFormats: ["jpg", "png"],
-  transformation: [{ width: 1000, crop: "fill" }],
+  folder : 'memorial',
+  allowedFormats : [ 'jpg', 'png' ],
+  transformation : [ {width : 1000, crop : 'fill'} ],
 });
 
-function parser() {
-  return multer({ storage });
-}
+function parser() { return multer({storage}); }
 
 module.exports = parser;
