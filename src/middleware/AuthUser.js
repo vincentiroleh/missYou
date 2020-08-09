c1eb38cfd80327cd0c1e029e9e3b981c29977e4e
Vincent Iroleh <irolehiroleh@gmail.com>
2020-08-10 00:10:06 +0100

    chore: refactor

--- src/middleware/AuthUser.js
+++ src/middleware/AuthUser.js
@@ -2,41 +2,6 @@
 /* eslint-disable consistent-return */
 const jwt = require('jsonwebtoken');
 
-function isAuthUser(req, res, next) {
-  if (req.isAuthenticated()) {
-    next();
-  }
-  return res.status(401).send({
-    status: 401,
-    message: 'Authorization required',
-  });
-}
-
-function isAdmin(req, res, next) {
-  const user = req.user;
-  if (user.role === 'Admin') {
-    next();
-  } else {
-    return res.status(401).send({
-      status: 401,
-      message: 'Authorization required',
-    });
-  }
-}
-
-function isClient(req, res, next) {
-  const user = req.user;
-  console.log(user);
-  if (user.role === 'Client') {
-    next();
-  } else {
-    return res.status(401).send({
-      status: 401,
-      message: 'Authorization required',
-    });
-  }
-}
-
 function verifyToken(req, res, next) {
   let token = req.headers.authorization;
   if (!token) {
@@ -62,8 +27,5 @@ function verifyToken(req, res, next) {
 }
 
 module.exports = {
-  isAuthUser,
-  isAdmin,
-  isClient,
   verifyToken,
 };
